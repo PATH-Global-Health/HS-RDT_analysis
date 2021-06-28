8ea55dc61e8edf541f0bac4e1b015534d9d6e31c
hannahslater <hannah.slater@imperial.ac.uk>
2021-06-28 09:53:50 -0700

    Add files via upload

---
+++ FIGURE_3.R
@@ -0,0 +1,87 @@
+library(readxl)
+library(dplyr)
+library(binom)
+library(rmeta)
+
+dd = readxl::read_xlsx("../data from articles.xlsx", sheet = "cross-sectional", col_types = c("text", rep("numeric", 13)))
+
+dd = data.frame(dd)
+
+cols <- c("gray30", "gold2","deepskyblue", "firebrick2", "plum4", "darkorange1", "darkslategray3", "gray60","burlywood3", "yellow4", "deepskyblue3", "lightsalmon2", "mediumpurple4", "deeppink2", "yellow3", "brown4","mediumseagreen" , "slateblue2",  "navy", "forestgreen")
+#springgreen4
+
+dd$col = cols
+
+# calculate the confidence intervals
+dd1 = dd[!is.na(dd$number.positiive.by.PCR),]
+
+bc_pcr = binom.confint(dd1$number.positiive.by.PCR, dd1$number.tested.by.PCR, methods = "exact")
+bc_pcr = data.frame(Study = dd1$Location, col = dd1$col, bc_pcr)
+
+bc_urdt = binom.confint(dd1$number.positiive.by.uRDT, dd1$number.tested.by.uRDT, methods = "exact")
+bc_urdt = data.frame(Study = dd1$Location, col = dd1$col, bc_urdt)
+
+# Wu relationship
+pcr_prev = seq(0, 1, l=101)
+
+LO = function(x){log(x/(1-x))}
+ULO = function(x){exp(x)/(1+exp(x))}
+rdt_prev = ULO(-0.968 + 1.186*LO(pcr_prev))
+sens = rdt_prev/pcr_prev
+
+# fit this functional form to HS-RDT data
+
+ff <- function(x){return(a0 + a1*LO(x))}
+
+y = bc_urdt$mean
+x = bc_pcr$mean
+
+fit = nls(LO(y) ~ a0 + a1*LO(x), start = list(a0 = -1, a1 =1))
+
+
+tiff("plots/FIG3_FINAL_1.tiff", width = 230,height=160,units="mm",res=300, compression="lzw")
+
+par(mar=c(4,4,2.5,1), mfrow=c(1,2),oma=c(8.2,0,0,0), xpd=FALSE)
+plot(bc_pcr$mean*100, bc_urdt$mean*100, pch=21, bg = "white", cex=1.5,
+     xlab = "PCR Prevalence (%)", ylab = "HS-RDT Prevalence (%)", las=1, xlim = c(0, 90), ylim=c(0,90))
+abline(v = seq(0, 100, by=10), col="grey80", lty=3)
+abline(h = seq(0, 100, by=10), col="grey80", lty=3)
+rect(-2,-2,6,6, col=adjustcolor("pink", 0.7), border=NA)
+
+abline(0,1, col="grey60", lty=5)
+segments(bc_pcr$lower*100, bc_urdt$mean*100, bc_pcr$upper*100, bc_urdt$mean*100, col=as.character(bc_pcr$col), lwd=2)
+segments(bc_pcr$mean*100, bc_urdt$lower*100, bc_pcr$mean*100, bc_urdt$upper*100, col=as.character(bc_pcr$col), lwd=2)
+points(bc_pcr$mean*100, bc_urdt$mean*100, pch=21, bg = adjustcolor(bc_pcr$col, 0.8), cex=1.5)
+
+lines(pcr_prev*100, rdt_prev*100, col="orange", lwd=2, lty=5)
+
+mtext("A", side = 3, adj=-0.1, font=2, cex=1.3)
+
+par(mar=c(4,4,2.5,1))
+plot(bc_pcr$mean*100, bc_urdt$mean*100, pch=21, bg = "white", cex=1.5,
+     xlab = "PCR Prevalence (%)", ylab = "HS-RDT Prevalence (%)", las=1, xlim = c(0, 6.5), ylim=c(0,6.5))
+rect(-2,-2,6,6, col=adjustcolor("pink", 0.4), border=NA)
+abline(0,1, col="grey60", lty=5)
+abline(v = seq(0, 6, by=1), col="grey80", lty=3)
+abline(h = seq(0, 6, by=1), col="grey80", lty=3)
+segments(bc_pcr$lower*100, bc_urdt$mean*100, bc_pcr$upper*100, bc_urdt$mean*100, col=as.character(bc_pcr$col), lwd=2)
+segments(bc_pcr$mean*100, bc_urdt$lower*100, bc_pcr$mean*100, bc_urdt$upper*100, col=as.character(bc_pcr$col), lwd=2)
+points(bc_pcr$mean*100, bc_urdt$mean*100, pch=21, bg = adjustcolor(bc_pcr$col, 0.8), cex=1.5)
+
+lines(pcr_prev*100, rdt_prev*100, col="orange", lwd=2, lty=5)
+
+
+mtext("B", side = 3, adj=-0.1, font=2, cex=1.3)
+
+par(xpd=NA)
+
+bc_pcr_leg = arrange(bc_pcr, mean)
+
+legend(-10, -2.0, paste0(bc_pcr_leg$Study)[1:9], pch=21, 
+       pt.bg = adjustcolor(bc_pcr_leg$col, 0.8)[1:9], pt.cex = 1.6, bty="n", cex=0.78)
+legend(-4, -2.0, paste0(bc_pcr_leg$Study)[10:18], pch=21, 
+       pt.bg = adjustcolor(bc_pcr_leg$col, 0.8)[10:18], pt.cex = 1.6, bty="n", cex=0.78)
+
+legend(2, -3.6, "Fitted line from Wu\net al. PCR vs. co-RDT\nprevalence relationship", cex=0.78, col="orange", lwd=2, lty=2, bty="n")
+
+dev.off()
---
+++ FIGURE_4.R
@@ -0,0 +1,134 @@
+# ALL CROSS-SECTIONAL DATA 
+# PREVALENCE PLOTS
+#install.packages("fmsb")
+#install.packages("rmeta")
+
+library(readxl)
+library(dplyr)
+library(binom)
+library(rmeta)
+
+dd = readxl::read_xlsx("../data from articles.xlsx", sheet = "cross-sectional", col_types = c("text", rep("numeric", 13)))
+
+dd = data.frame(dd)
+
+cols <- c("gray30", "gold2","deepskyblue", "firebrick2", "plum4", "darkorange1", "darkslategray3", "gray60","burlywood3", "yellow4", "deepskyblue3", "lightsalmon2", "mediumpurple4", "deeppink2", "yellow3", "brown4","mediumseagreen" , "slateblue2", "navy", "forestgreen")
+#springgreen4
+
+dd$col = cols
+
+
+# plot 2 sensitivity of the HS-RDT
+
+dd3 = dd[!is.na(dd$number.positiive.by.PCR),]
+dd3 = arrange(dd3, desc(number.positiive.by.PCR))
+
+dd3$pch_urdt = 16
+dd3$pch_urdt[is.na(dd3$number.of.PCR.positives.that.are.also.RDT.positive)] = 17
+
+
+tiff("plots/FIG4_FINAL_1.tiff", width = 260,height=160,units="mm",res=300, compression="lzw")
+
+par(mar = c(4,4,2,20), xpd=FALSE)
+plot(dd3$PCR.prevalence,
+     dd3$number.of.PCR.positives.that.are.also.uRDT.positive/dd3$number.positiive.by.PCR*100,
+     ylab = "Sensitivity (%)", xlab = "PCR prevalence (%)", las=1, ylim=c(0,100), col = dd3$col, pch=dd3$pch_urdt,lwd=2,
+     cex=1.8)
+
+mean_urdt = weighted.mean(dd3$number.of.PCR.positives.that.are.also.uRDT.positive/dd3$number.positiive.by.PCR*100,
+                          dd3$number.positiive.by.PCR)
+
+dd3_rdt = dd3[!is.na(dd3$number.positiive.by.RDT),]
+
+mean_rdt = weighted.mean(dd3_rdt$number.of.PCR.positives.that.are.also.RDT.positive/dd3_rdt$number.positiive.by.PCR*100,
+                         dd3_rdt$number.positiive.by.PCR)
+
+# abline(h = mean_urdt, lwd=4, col="grey60")
+# abline(h = c(mean_rdt+0.4, mean_rdt-0.4), lwd=1, col="grey60")
+abline(h = seq(0, 100, by=10), lty=3, col="grey80")
+abline(v = seq(0, 100, by=10), lty=3, col="grey80")
+
+points(dd3$PCR.prevalence,
+       dd3$number.of.PCR.positives.that.are.also.uRDT.positive/dd3$number.positiive.by.PCR*100,
+       lwd=3, col=dd3$col, cex=1.8, pch=dd3$pch_urdt)
+
+points(dd3$PCR.prevalence,
+    dd3$number.of.PCR.positives.that.are.also.RDT.positive/dd3$number.positiive.by.PCR*100,
+    lwd=3, col=dd3$col, cex=1.8)
+
+segments(dd3$PCR.prevalence, 
+         dd3$number.of.PCR.positives.that.are.also.RDT.positive/dd3$number.positiive.by.PCR*100,
+         dd3$PCR.prevalence,
+         dd3$number.of.PCR.positives.that.are.also.uRDT.positive/dd3$number.positiive.by.PCR*100)
+
+dd3$urdt_sens = dd3$number.of.PCR.positives.that.are.also.RDT.positive/dd3$number.positiive.by.PCR*100
+
+mm1 = glm(cbind(round(number.of.PCR.positives.that.are.also.uRDT.positive,0), 
+          number.positiive.by.PCR - number.of.PCR.positives.that.are.also.uRDT.positive) ~ PCR.prevalence, data = dd3, family = "binomial")
+
+summary(mm1)
+
+xxseq = seq(0, 100, by=1)
+preds = predict.glm(mm1, newdata = data.frame(PCR.prevalence = xxseq), se.fit = TRUE)
+
+ilink <- family(mm1)$linkinv
+
+lines(xxseq, ilink(preds$fit)*100, lwd=2, col="grey30")
+polygon(c(xxseq, rev(xxseq)), 
+        c(ilink(preds$fit + 2*preds$se.fit), rev(ilink(preds$fit - 2*preds$se.fit)))*100,
+        border=NA, col = adjustcolor("grey", 0.4))
+
+
+mm2 = glm(cbind(round(number.of.PCR.positives.that.are.also.RDT.positive,0), 
+                number.positiive.by.PCR - number.of.PCR.positives.that.are.also.RDT.positive) ~ 
+            PCR.prevalence, data = dd3, family = "binomial")
+
+summary(mm2)
+
+xxseq = seq(0, 100, by=1)
+preds2 = predict.glm(mm2, newdata = data.frame(PCR.prevalence = xxseq), se.fit = TRUE)
+
+ilink <- family(mm2)$linkinv
+
+lines(xxseq, ilink(preds2$fit)*100, lwd=2, col="grey30", lty=5)
+polygon(c(xxseq, rev(xxseq)), 
+        c(ilink(preds2$fit + 2*preds2$se.fit), rev(ilink(preds2$fit - 2*preds2$se.fit)))*100,
+        border=NA, col = adjustcolor("grey", 0.4))
+
+
+dd3_fig = arrange(dd3, PCR.prevalence)
+
+par(xpd=NA)
+legend(86, 98, dd3_fig$Location, pch = dd3_fig$pch_urdt, col=dd3_fig$col, pt.cex = 1.4, title = "Study", bty="n")
+
+
+legend(89, 11, c("HS-RDT estimated sensitivity", "Co-RDT estimated sensitivity"), col = "grey30", lwd=2,
+       lty = c(1, 2))
+
+# legend(89, 16, c(paste0("HS-RDT (",round(mean_urdt,1),"%)"), paste0("co-RDT (",round(mean_rdt,1),"%)")), col="grey60", lwd=c(4,5), title = "")
+# legend(89, 16, c(paste0("HS-RDT (",round(mean_urdt,1),"%)"), paste0("co-RDT (",round(mean_rdt,1),"%)")), col=c(NA, "white"), lwd=c(4,3), bty="n", title = "Mean sensitivity")
+
+dev.off()
+
+
+# 
+# weighted.ttest.ci <- function(x, weights, conf.level = 0.95) {
+#   require(Hmisc)
+#   nx <- length(x)
+#   df <- nx - 1
+#   vx <- wtd.var(x, weights, normwt = TRUE) ## From Hmisc
+#   mx <- weighted.mean(x, weights)
+#   stderr <- sqrt(vx/nx)
+#   tstat <- mx/stderr ## not mx - mu
+#   alpha <- 1 - conf.level
+#   cint <- qt(1 - alpha/2, df)
+#   cint <- tstat + c(-cint, cint)
+#   cint * stderr
+# }
+# 
+# 
+# weighted.ttest.ci(x = dd3$number.of.PCR.positives.that.are.also.uRDT.positive/dd3$number.positiive.by.PCR*100,
+#                   weights = dd3$number.positiive.by.PCR)
+# 
+# weighted.ttest.ci(x = dd3_rdt$number.of.PCR.positives.that.are.also.RDT.positive/dd3_rdt$number.positiive.by.PCR*100,
+#                   weights = dd3_rdt$number.positiive.by.PCR)
